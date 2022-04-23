05f6ff378cdf8a87ea7d642dbfce34c04b869054
Guido van de Riet <guido@chainels.com>
2022-04-24 01:25:30 +0200

    added cloud store firebase config

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,22 @@
 import * as S from "./app.style";
+import { db, getQuestions } from "../../firebase";
 
 // Components
 import QuestionCard from "../questionCard/QuestionCard";
 import GlobalStyle from "../style/GlobalStyle";
+import { useEffect } from "react";
+import { child, get, onValue, ref } from "firebase/database";
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      const questions = await getQuestions(db);
+      console.log({ questions });
+    };
+
+    getData();
+  }, []);
+
   return (
     <S.App>
       <GlobalStyle />
